12747ed7ec85781a90d6edba97e7e56893b01c8f
Mateus Lisboa <chart010@hotmail.com>
2018-04-17 17:01:42 -0300

    inciiando php

---
+++ modulos/menu.php
@@ -0,0 +1,6 @@
+<nav class="mdl-navigation">
+  <a class="mdl-navigation__link" href="#" id="show-dialog"><i class="material-icons">person_pin</i> Entrar</a>
+  <a class="mdl-navigation__link" href="sobre.html"><i class="material-icons">info_outline</i> Sobre</a>
+  <a class="mdl-navigation__link" href="ajuda.html"><i class="material-icons">help</i> Ajuda</a>
+  <a class="mdl-navigation__link" href="versao.html"><i class="material-icons">history</i> Versão 1.1.0</a>      
+</nav>
